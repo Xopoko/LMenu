776a890e571ce8bd410931789d9980c3745940fe
Horoko <horokko@mail.ru>
2024-10-28 22:50:28 +0100

    feat(options.html): translate extension settings page to English for better accessibility feat(options.js): update alert messages to English for consistency with UI language style(resultWindow.css): adjust button positioning and styles for improved UI appearance and responsiveness

--- options.js
+++ options.js
@@ -4,11 +4,11 @@ document.getElementById('saveButton').addEventListener('click', function() {
     const apiKey = document.getElementById('apiKey').value.trim();
     if (apiKey) {
         chrome.storage.sync.set({ openaiApiKey: apiKey }, function() {
-            alert('API ключ сохранен.');
+            alert('API key is saved.');
             document.getElementById('apiKey').value = '';
         });
     } else {
-        alert('Пожалуйста, введите API ключ.');
+        alert('Please enter a valid API key.');
     }
 });
 
